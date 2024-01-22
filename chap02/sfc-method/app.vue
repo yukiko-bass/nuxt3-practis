6be3cbdd8d1b7a00583c085f2a2af41c03281a70
yukiko-bass <springcomesagain129@gmail.com>
2024-01-22 12:07:04 +0900

    2-1. SFCへの記述の基本

---
+++ chap02/sfc-method/app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const msg = ref("まだ");
+const onButtonClick = (label: string, event: Event): void => {
+  const target = event.target as HTMLButtonElement;
+  const text = target.innerText;
+  msg.value = `${label}  と ${text}`;
+}
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <button v-on:click="onButtonClick('Hello', $event)">こんにちは</button>
+</template>
