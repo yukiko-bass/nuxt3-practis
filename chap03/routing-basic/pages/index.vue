71a5e386fbc26697d7d0e68ef246609eef208ce9
yukiko-bass <springcomesagain129@gmail.com>
2024-01-28 10:34:11 +0900

    3.1.3 add index.vue

---
+++ chap03/routing-basic/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+    TOP
+  </h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>
+        TOP
+      </li>
+    </ul>
+  </nav>
+  <section>
+    <p>会員登録はこちら</p>
+  </section>
+</template>
