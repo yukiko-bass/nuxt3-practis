bf056e74a892da4086bec13ccf266caf12e9a074
yukiko-bass <springcomesagain129@gmail.com>
2024-05-11 18:40:08 +0900

    3.3.3-4. ネストされたルーティング

---
+++ chap03/routing-nested/pages/member/memberList.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { Member } from "@/interfaces";
+
+const memberList = useState<Map<number, Member>>("memberList");
+</script>
+
+<template>
+  <h1>会員管理</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>
+        <NuxtLink v-bind:to="{ name: 'index' }">TOP</NuxtLink>
+      </li>
+      <li>会員リスト</li>
+    </ul>
+  </nav>
+  <section>
+    <h2>
+      会員リスト
+    </h2>
+    <p>
+      新規登録は<NuxtLink v-bind:to="{name: 'member-memberList-memberAdd'}">こちら</NuxtLink>から
+    </p>
+    <section>
+      <ul>
+        <li v-for="[id, member] in memberList" v-bind:key="id">
+          <NuxtLink v-bind:to="{ name: 'member-memberList-memberDetail-id', params: { id: id } }">
+            IDが {{ id }} の {{ member.name }} さん
+          </NuxtLink>
+        </li>
+      </ul>
+    </section>
+    <NuxtPage />
+  </section>
+</template>
